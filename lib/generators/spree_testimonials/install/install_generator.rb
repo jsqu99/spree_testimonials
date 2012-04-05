d53e2337d800b4c39406eb2f82ee0021399b3c05
Jeff Squires <jeff.squires@gmail.com>
2012-04-05 13:13:49 -0400

    removes bogus placeholder files

--- lib/generators/spree_testimonials/install/install_generator.rb
+++ lib/generators/spree_testimonials/install/install_generator.rb
@@ -2,14 +2,8 @@ module SpreeTestimonials
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
-      def add_javascripts
-        append_file "app/assets/javascripts/store/all.js", "//= require store/spree_testimonials\n"
-        append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_testimonials\n"
-      end
-
       def add_stylesheets
         inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_testimonials\n", :before => /\*\//, :verbose => true
-        inject_into_file "app/assets/stylesheets/admin/all.css", " *= require admin/spree_testimonials\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
