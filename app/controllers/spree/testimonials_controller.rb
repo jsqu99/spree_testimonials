2b059c02cf4cff16eafd9bed1cba6f77a07da3b1
Jeff Squires <jeff.squires@gmail.com>
2012-05-08 14:30:34 -0400

    fixes testimonials 'create'

--- app/controllers/spree/testimonials_controller.rb
+++ app/controllers/spree/testimonials_controller.rb
@@ -1,14 +1,26 @@
 module Spree
   class TestimonialsController < Spree::BaseController
-    resource_controller
+
+    respond_to :html
+
+    def create
+      @testimonial = Testimonial.new(params[:testimonial])
+
+      if @testimonial.save
+        respond_with(@testimonial) do |format|
+          format.html { redirect_to :root, :notice => "Thank you!.  This testimonial will be displayed shortly on our testimonials page"}
+        end
+      else
+        respond_with(@testimonial) do |format|
+          format.html { render :action => "new" }
+        end
+      end
+    end
 
     def index
       @testimonials = Testimonial.order("created_at DESC")
       # paginate(:per_page => 30, :page => params[:page])
     end
 
-    create.response do |format|
-      format.html { redirect_to :root, :notice => "Thank you!.  This testimonial will be displayed shortly on our testimonials page"}
-    end
   end
 end
