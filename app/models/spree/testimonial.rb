b4514dd5baba56ffa4a99a6e329cfeb0b743a872
Brice Sanchez <contact@brice-sanchez.com>
2013-11-25 16:30:02 -0500

    Add title field and clean up code

--- app/models/spree/testimonial.rb
+++ app/models/spree/testimonial.rb
@@ -2,9 +2,10 @@ require 'spree/core/validators/email'
 
 module Spree
   class Testimonial < ActiveRecord::Base
-    attr_accessible :name, :body, :email, :approved
+    attr_accessible :name, :title, :body, :email, :approved
 
     validates :name,  presence: true
+    validates :title, presence: true
     validates :body,  presence: true
     validates :email, presence: true
     validates :email, email: true
